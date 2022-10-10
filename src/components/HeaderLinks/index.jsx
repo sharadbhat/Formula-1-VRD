13cd5a2d18683b1a513d535b4e9d10dae7f6853d
sharadbhat <sharad.mbhat@gmail.com>
2022-10-09 23:35:40 -0600

    adding support for fullscreen

--- src/components/HeaderLinks/index.jsx
+++ src/components/HeaderLinks/index.jsx
@@ -1,36 +1,55 @@
-import { Button } from '@mantine/core'
+import { Button, Tooltip } from '@mantine/core'
+import { useFullscreen } from '@mantine/hooks';
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// Icons
+import { faUpRightAndDownLeftFromCenter, faDownLeftAndUpRightToCenter } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
+// CSS
 import './styles.css'
 
 const HeaderLinks = () => {
-  return (
-    <div className='linksContainer'>
-        <Button
-            variant='outline'
-            color='gray'
-            component={Link}
-            to='/processBook'
-            className='item'
-        >
-            Process Book
-        </Button>
-        <Button
-            variant='outline'
-            color='gray'
-            compact
-            className='githubButton item'
-            component='a'
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://github.com/sharadbhat/Formula-1-VRD'
-        >
-            <FontAwesomeIcon color='white' icon={faGithub} size="xl" />
-        </Button>
-    </div>
-  )
+    const { toggle, fullscreen } = useFullscreen();
+    return (
+        <div className='linksContainer'>
+            <Button
+                variant='outline'
+                color='gray'
+                component={Link}
+                to='/processBook'
+                className='item'
+            >
+                Process Book
+            </Button>
+            <Tooltip label='Github Repository'>
+                <Button
+                    variant='outline'
+                    color='gray'
+                    compact
+                    className='githubButton item'
+                    component='a'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://github.com/sharadbhat/Formula-1-VRD'
+                >
+                    <FontAwesomeIcon color='white' icon={faGithub} size='xl' />
+                </Button>
+            </Tooltip>
+            <Tooltip label={fullscreen ? 'Minimize' : 'Maximize'}>
+                <Button
+                    variant='outline'
+                    color='gray'
+                    compact
+                    className='githubButton item'
+                    onClick={toggle}
+                    >
+                    <FontAwesomeIcon color='white' icon={fullscreen ? faDownLeftAndUpRightToCenter : faUpRightAndDownLeftFromCenter} size='lg' />
+                </Button>
+            </Tooltip>
+        </div>
+    )
 }
 
 export default HeaderLinks
