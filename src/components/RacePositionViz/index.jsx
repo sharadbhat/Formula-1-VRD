63907d036b02c90d52cd6a5dcd85bf0cfadb35bb
sharadbhat <sharad.mbhat@gmail.com>
2022-10-15 02:14:21 -0600

    example race

---
+++ src/components/RacePositionViz/index.jsx
@@ -0,0 +1,64 @@
+import * as d3 from 'd3'
+
+// Utils
+import { useD3 } from '../../utils/useD3'
+
+const RacePositionViz = ({ data }) => {
+    const svgWidth = 1000
+    const svgHeight = 500
+
+    const margin = 30
+
+    const ref = useD3(svg => {
+        let xScale = d3.scaleLinear()
+            .domain(d3.extent(data.map(d => +d.lap)))
+            .range([0, svgWidth - margin])
+
+        let yScale = d3.scaleLinear()
+            .domain(d3.extent(data.map(d => +d.position)))
+            .range([0, svgHeight - margin])
+
+        svg.select('#xAxis')
+            .attr('transform', `translate(${margin}, ${svgHeight - margin})`)
+            .transition()
+            .duration(500)
+            .call(d3.axisBottom(xScale))
+
+        svg.select('#yAxis')
+            .attr('transform', `translate(${margin}, 0)`)
+            .transition()
+            .duration(500)
+            .call(d3.axisLeft(yScale))
+        
+        const groupedData = d3.group(data, d => +d.driverId)
+
+        let colorScale = d3.scaleOrdinal()
+            .domain(Array.from(groupedData.keys()))
+            .range(['#32964d', '#21f0b6', '#195036', '#ace1b7', '#0a7fb2', '#9ab9f9', '#82269b', '#fa79f5', '#6e446b', '#20d8fd', '#135ac2', '#faafe3', '#7d0af6', '#bae342', '#6e3901', '#fe8f06', '#a20655', '#fb8c77', '#d02023', '#d8c3b4', '#738a69', '#67f059', '#e0c645'])
+
+        svg.select('#content')
+            .selectAll('path')
+            .data(groupedData)
+            .enter()
+            .append('path')
+            .attr('fill', 'none')
+            .attr('stroke', d => colorScale(+d[0]))
+            .attr('stroke-width', 1.5)
+            .attr('d', d => {
+                return d3.line()
+                    .x(d => margin + xScale(+d.lap))
+                    .y(d => yScale(+d.position))
+                (d[1])
+            })
+    }, [data.length])
+
+    return (
+        <svg ref={ref} style={{ width: svgWidth, height: svgHeight }}>
+            <g id='xAxis' />
+            <g id='yAxis' />
+            <g id='content' />
+        </svg>
+    )
+}
+
+export default RacePositionViz
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react'
+import { LoadingOverlay } from '@mantine/core'
+import * as d3 from 'd3'
+
+// Components
+import RacePositionViz from '../../components/RacePositionViz'
+
 const Home = () => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  let raceId = '1073'
+
+  useEffect(() => {
+    function fetchData() {
+      d3.csv('../../data/lap_times.csv').then(d => {
+        setData(d.filter(row => row.raceId === raceId).map(row => ({ driverId: +row.driverId, lap: +row.lap, position: +row.position })))
+        setLoading(false)
+      })
+    }
+    fetchData();
+  },[raceId])
+
   return (
-    'Home'
+    <>
+      <LoadingOverlay visible={loading} overlayBlur={2} />
+      <RacePositionViz data={data} />
+    </>
   )
 }
 
