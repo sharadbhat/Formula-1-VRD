f7e3d85bc2c350c995a48a2dd07ef7899e14e1ed
alexliebig <alexcliebig@gmail.com>
2022-11-20 02:01:15 -0700

    added scraped data to website

--- src/components/RaceScrapeViz/index.jsx
+++ src/components/RaceScrapeViz/index.jsx
@@ -1,5 +1,5 @@
 import { forwardRef } from 'react'
-import { Select, Text, Card, Group, Badge, Button, Image } from '@mantine/core'
+import { Select, Text, Card, Group, Badge, Button, Image, Grid} from '@mantine/core'
 import * as d3 from 'd3'
 
 // Utils
@@ -10,27 +10,30 @@ import { useD3 } from '../../utils/useD3'
 const RaceScrapeViz = ({raceList, scrapedRaceList}) => {
     const selectedRace = useGlobalStore((state) => state.selectedRace);
     if (scrapedRaceList){
-        console.log(raceList);
         return(
             <div>
-                {raceList.map(function(race, i){
-                    return (
-                        <Card shadow="sm" p="md" radius="md" withBorder>
-                            <Card.Section>
-                                <Image
-                                src={scrapedRaceList[race.circuitId].img_url}
-                                alt={race.name}
-                                />
-                            </Card.Section>
-                            <Group position="apart" mt="md" mb="xs">
-                                <Text weight={500}>{race.name}</Text>
-                            </Group>
-                            <Text size="sm" color="dimmed">
-                                {scrapedRaceList[race.circuitId].description}
-                            </Text>
-                        </Card>
-                    );
-                })}
+                <Grid>
+                    {raceList.map(function(race, i){
+                        return (
+                            <Grid.Col span={3}>
+                                <Card shadow="sm" p="md" radius="md" withBorder>
+                                    <Card.Section>
+                                        <Image
+                                        src={scrapedRaceList[race.circuitId].img_url}
+                                        alt={race.name}
+                                        />
+                                    </Card.Section>
+                                    <Group position="apart" mt="md" mb="xs">
+                                        <Text weight={500}>{race.name}</Text>
+                                    </Group>
+                                    <Text size="sm" color="dimmed">
+                                        {scrapedRaceList[race.circuitId].description}
+                                    </Text>
+                                </Card>
+                            </Grid.Col>
+                        );
+                    })}
+                </Grid>
             </div>
         )
     }
