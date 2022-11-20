4acb4bf7195c16a1daa8c42c374435e44c9e01ba
alexliebig <alexcliebig@gmail.com>
2022-11-19 21:37:33 -0700

    added data scraper

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { LoadingOverlay } from '@mantine/core'
+import { Card } from '@mantine/core'
 
 // Components
 import OvertakeDensityViz from '../../components/OvertakeDensityViz'
