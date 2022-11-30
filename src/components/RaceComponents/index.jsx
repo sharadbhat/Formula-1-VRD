2e39914042de626c07ccb5bdf0c4037b807beacf
sharadbhat <sharad.mbhat@gmail.com>
2022-11-30 01:35:31 -0700

    chevron right

--- src/components/RaceComponents/index.jsx
+++ src/components/RaceComponents/index.jsx
@@ -6,10 +6,15 @@ import OvertakeDensityViz from '../OvertakeDensityViz'
 import RacePositionViz from '../RacePositionViz'
 import DriverDetails from '../DriverDetails'
 
+// Utils
+import useGlobalStore from '../../utils/store'
+
 // Icons
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+import { faCircleChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const RaceComponents = ({ racePositionData, driverFinishPositions, lapTimeData }) => {
+    const selectedDrivers = useGlobalStore(state => state.selectedDrivers)
     return (
         <>
             <RacePositionViz
@@ -35,18 +40,32 @@ const RaceComponents = ({ racePositionData, driverFinishPositions, lapTimeData }
                     <OvertakeDensityViz data={racePositionData} />
                 </div>
                 <div style={{ width: 750, marginRight: 10 }}>
-                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                        <Text style={{ fontWeight: 700, marginRight: 20 }}>Driver Profile(s)</Text>
-                        <HoverCard width={280}>
-                            <HoverCard.Target>
-                                <FontAwesomeIcon color='white' style={{ paddingTop: 5 }} icon={faQuestionCircle} size='sm' />
-                            </HoverCard.Target>
-                            <HoverCard.Dropdown>
-                                <Text size='sm'>
-                                    This sections shows a card for each selected driver.
-                                </Text>
-                            </HoverCard.Dropdown>
-                        </HoverCard>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={{ fontWeight: 700, marginRight: 20 }}>Driver Profile(s)</Text>
+                            <HoverCard width={280}>
+                                <HoverCard.Target>
+                                    <FontAwesomeIcon color='white' style={{ paddingTop: 5 }} icon={faQuestionCircle} size='sm' />
+                                </HoverCard.Target>
+                                <HoverCard.Dropdown>
+                                    <Text size='sm'>
+                                        This sections shows a card for each selected driver.
+                                    </Text>
+                                </HoverCard.Dropdown>
+                            </HoverCard>
+                        </div>
+                        {selectedDrivers.length > 2
+                            ? <HoverCard>
+                                <HoverCard.Target>
+                                    <FontAwesomeIcon color='white' style={{ paddingTop: 5 }} icon={faCircleChevronRight} size='sm' />
+                                </HoverCard.Target>
+                                <HoverCard.Dropdown>
+                                    <Text size='sm'>
+                                        Scroll to the right to view all driver cards
+                                    </Text>
+                                </HoverCard.Dropdown>
+                            </HoverCard>
+                            : null}
                     </div>
                     <DriverDetails />
                 </div>
